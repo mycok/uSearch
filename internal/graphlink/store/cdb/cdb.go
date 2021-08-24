6ca860a915cb688906117c002aa70be122a9157e
myko <awesomeme155@gmail.com>
2021-08-25 01:24:47 +0300

    feat: cockroachDB repository setup
    - add cdb.go file
    - define link queries
    - define a CockroachDBGraph type to hold a cdb connection instance
    - define a NewCockroachDBGraph function to initialize and return a CockroachDBGraph instance
    - define a Close() method to be used to close the cdb connection instance

---
+++ internal/graphlink/store/cdb/cdb.go
@@ -0,0 +1,64 @@
+package cdb
+
+import (
+	"database/sql"
+)
+
+var (
+	upsertLinkQuery = `
+					INSERT INTO links (url, retrieved_at)
+					VALUES ($1, $2)
+					ON CONFLICT (url) DO UPDATE SET retrieved_at=GREATEST(links.retrieved_at, $2)
+					RETURNING id, retrieved_at
+					`
+	
+	findLinkQuery = "SELECT id, url, retrieved_at FROM links WHERE id=$1"
+
+	linksInPartitionsQuery = `
+							SELECT id, url, retrieved_at FROM links 
+							WHERE id >= $1
+							AND id < $2
+							AND retrieved_at < $3
+							`
+	
+	upsertEdgeQuery = `
+					INSERT INTO edges (src, dest, updated_at)
+					VALUES ($1, $2, NOW())
+					ON CONFLICT (src, dest) DO UPDATE SET updated_at=NOW()
+					RETURNING id, updated_at
+					`
+
+	edgesInPartionsQuery = `
+							SELECT id, src, dest, updated_at FROM edges
+							WHERE src >= $1
+							AND src < $2
+							AND updated_at < $3
+							`
+	
+	removeStaleEdgesQuery = "DELETE FROM edges WHERE src=$1 AND updated_at < $2"			
+)
+
+// Compile-time check for ensuring CockroachDbGraph implements Graph.
+// var _ graph.Graph = (*CockroachDBGraph)(nil)
+
+// CockroachDBGraph implements a Graph concrete type that persists its links
+//  and edges to a cockroachdb instance.
+type CockroachDBGraph struct {
+	db *sql.DB
+}
+
+// NewCockroachDbGraph returns a CockroachDbGraph instance that connects to the cockroachdb
+// instance specified by dsn.
+func NewCockroachDbGraph(dsn string) (*CockroachDBGraph, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	return &CockroachDBGraph{db: db}, nil
+}
+
+// Close terminates the connection to the cockroachdb instance.
+func (c *CockroachDBGraph) Close() error {
+	return c.db.Close()
+}
