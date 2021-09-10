3c4781b310cc9206dce0d105d12755a76243a267
myko <awesomeme155@gmail.com>
2021-09-10 15:48:42 +0300

    feat: pipeline stage strategies: dynamic-worker-pool
    - add dynamic-worker-pool implementation

--- internal/pipeline/stage.go
+++ internal/pipeline/stage.go
@@ -63,6 +63,10 @@ func (r *fifo) Run(ctx context.Context, params StageParams) {
 	}
 }
 
+
+// Compile-time check for ensuring fixedWorkerPool implements StageRunner.
+var _ StageRunner = (*fixedWorkerPool)(nil)
+
 // fixedWorkerPool spins up a preconfigured number of workers and distributes incoming
 // payloads among them 
 type fixedWorkerPool struct {
@@ -102,6 +106,91 @@ func (r *fixedWorkerPool) Run(ctx context.Context, params StageParams) {
 		}(i)
 	}
 
+	// Wait for all running worker goroutines to complete / return before allowing
+	// the Run method to return.
 	wg.Wait()
+}
+
+
+// Compile-time check for ensuring dynamicWorkerPool implements StageRunner.
+var _ StageRunner = (*dynamicWorkerPool)(nil)
+
+type dynamicWorkerPool struct {
+	proc Processor
+	tokenPool chan struct{}
+}
+
+// DynamicWorkerPool returns a StageRunner that maintains a dynamic worker pool
+// that can scale up to maxWorkers for processing incoming inputs in parallel
+// and emitting their outputs to the next stage.
+func DynamicWorkerPool(proc Processor, maxNumOfWorkers int) StageRunner {
+	if maxNumOfWorkers <= 0 {
+		panic("DynamicWorkerPool: maxNumOfWorkers must be > 0")
+	}
+
+	tokenPool := make(chan struct{}, maxNumOfWorkers)
+	for i := 0; i < maxNumOfWorkers; i++ {
+		tokenPool <- struct{}{}
+	}
+
+	return &dynamicWorkerPool{
+		proc: proc,
+		tokenPool: tokenPool,
+	}
+}
+
+func (r *dynamicWorkerPool) Run(ctx context.Context, params StageParams) {
+	stop:
+		for {
+			select {
+			case <-ctx.Done():
+				break stop
+			case payloadIn, ok := <-params.Input():
+				if !ok {
+					break stop
+				}
+
+				var token struct{}
+				select {
+				case token = <-r.tokenPool:
+				case <-ctx.Done():
+					break stop
+				}
+
+				go func(payloadIn Payload, token struct{}) {
+					defer func() {
+						r.tokenPool <- token
+					}()
+
+					payloadOut, err := r.proc.Process(ctx, payloadIn)
+					if err != nil {
+						wrappedError := fmt.Errorf("pipeline stage %d: %w", params.StageIndex(), err)
+						shouldEmitError(wrappedError, params.Error())
+
+						return
+					}
+
+					// If the processor did not output a payload for the
+					// next stage there is nothing we need to do.
+					if payloadOut == nil {
+						payloadIn.MarkAsProcessed()
+
+						return // Discard the payload.
+					}
+
+					// Output processed data.
+					select {
+					case params.Output() <- payloadOut:
+					case <-ctx.Done():
+					}
+
+				}(payloadIn, token)
+			}
+		}
+
+		// Wait for all workers to exit by trying to empty the token pool.
+		for i := 0; i < cap(r.tokenPool); i++ {
+			<-r.tokenPool
+		}
 }
 
