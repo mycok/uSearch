3fbd250f2d8da4e1fb0c18d74d43dd27e3b06321
myko <awesomeme155@gmail.com>
2021-09-11 02:00:49 +0300

    feat: pipeline stage stratgies: broadcast
    - add broadcast stage runner implementation

--- internal/pipeline/stage.go
+++ internal/pipeline/stage.go
@@ -188,9 +188,109 @@ func (r *dynamicWorkerPool) Run(ctx context.Context, params StageParams) {
 			}
 		}
 
-		// Wait for all workers to exit by trying to empty the token pool.
-		for i := 0; i < cap(r.tokenPool); i++ {
-			<-r.tokenPool
+	// Wait for all workers to exit by trying to empty the token pool.
+	for i := 0; i < cap(r.tokenPool); i++ {
+		<-r.tokenPool
+	}
+}
+
+
+// Compile-time check for ensuring fixedWorkerPool implements StageRunner.
+var _ StageRunner = (*broadcast)(nil)
+
+type broadcast struct {
+	fifos []StageRunner
+}
+
+// Broadcast returns a StageRunner that passes a copy of each incoming payload
+// to all specified processors and emits their outputs to the next stage.
+func Broadcast(procs ...Processor) StageRunner {
+	if len(procs) == 0 {
+		panic("Broadcast: at least one processor must be specified")
+	}
+
+	fifos := make([]StageRunner, len(procs))
+	for i, p := range procs {
+		fifos[i] = FIFO(p)
+	}
+
+	return &broadcast{
+		fifos: fifos,
+	}
+}
+
+func (r *broadcast) Run(ctx context.Context, params StageParams) {
+	var (
+		wg sync.WaitGroup
+		inChs = make([]chan Payload, len(r.fifos))
+	)
+
+	// Start each FIFO in a go-routine. Each FIFO gets its own dedicated
+	// input channel and the shared output channel passed to Run.
+	for i := 0; i < len(r.fifos); i++ {
+		wg.Add(1)
+
+		inChs[i] = make(chan Payload)
+
+		go func(fifoIndex int) {
+			fifoParams := &workerParams{
+				stage: params.StageIndex(),
+				inCh:  inChs[fifoIndex],
+				outCh: params.Output(),
+				errCh: params.Error(),
+			}
+			
+			r.fifos[fifoIndex].Run(ctx, fifoParams)
+
+			wg.Done()
+		}(i)
+	}
+
+	// This is the main loop and it runs constantly until the [params.input] channel
+	// is closed or the context gets cancelled.
+	done:
+		for {
+			// Read incoming payloads then pass them to each FIFO instance.
+			// The select block of code blocks until either the payload is read from
+			// the input channel or the context gets cancelled.
+			select {
+			case <-ctx.Done():
+				break done
+			case payload, ok := <-params.Input():
+				if !ok {
+					break done
+				}
+
+				// This loop writes payloads to each dedicated FIFO input channel.
+				// After writing payloads to all FIFO input channels, a new iteration
+				// of the main begins.
+				for i := len(r.fifos) - 1; i >= 0; i-- {
+					// Since each FIFO might modify the payload, in order
+					// avoid data races we need to make a copy of
+					// the payload for all FIFOs except the first.
+					var fifoPayload = payload
+					if i != 0 {
+						fifoPayload = payload.Clone()
+					}
+
+					select {
+					case <-ctx.Done():
+						break done
+					case inChs[i] <- fifoPayload:
+					}
+
+				}
+			}
 		}
+
+	// This loop runs after the main loop has exited.
+	// Signal each of the fifo stage runner instances to shut down by closing their 
+	// dedicated input channels.
+	for _, ch := range inChs {
+		close(ch)
+	}
+
+	// Wait for all the go-routines to exit before allowing the Run method to return.
+	wg.Wait()
 }
 
