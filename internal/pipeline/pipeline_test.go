db4c7b8ce80991604fc4843234b74a52420cee13
myko <awesomeme155@gmail.com>
2021-09-11 20:47:29 +0300

    chore: pipeline tests: test-pipeline-dataflow
    - add source, sink and testStage stubs and implementations
    - add TestDataFlow test case

---
+++ internal/pipeline/pipeline_test.go
@@ -0,0 +1,160 @@
+package pipeline_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/mycok/uSearch/internal/pipeline"
+
+	check "gopkg.in/check.v1"
+)
+
+// Initialize and register an instance of the PipelineTestSuite to be
+// executed by check testing package.
+var _ = check.Suite(new(PipelineTestSuite))
+
+type PipelineTestSuite struct {}
+
+func Test(t *testing.T) {
+	check.TestingT(t)
+}
+
+func (s *PipelineTestSuite) TestDataFlow(c *check.C) {
+	stages := make([]pipeline.StageRunner, 10)
+	for i := 0; i < len(stages); i++ {
+		stages[i] = testStage{c: c}
+	}
+
+	src := &sourceStab{data: stringPayloads(3)}
+	sink := new(sinkStub)
+
+	p := pipeline.New(stages...)
+	err := p.Process(context.TODO(), src, sink)
+
+	c.Assert(err, check.IsNil)
+	c.Assert(sink.data, check.DeepEquals, src.data)
+	assertAllProcessed(c, src.data)
+}
+
+// Test setup helpers
+func assertAllProcessed(c *check.C, payloads []pipeline.Payload) {
+	for i, p := range payloads {
+		payload := p.(*stringPayload)
+		c.Assert(payload.processed, check.Equals, true, check.Commentf("payload %d not processed", i))
+	}
+}
+
+type testStage struct {
+	c *check.C
+	dropPayloads bool
+	err error
+}
+
+func (s testStage) Run(ctx context.Context, params pipeline.StageParams) {
+	defer func() {
+		s.c.Logf("[stage %d] exiting", params.StageIndex())
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case payload, ok := <-params.Input():
+			if !ok {
+				return
+			}
+
+			s.c.Logf("[stage %d] received payload: %v", params.StageIndex(), payload)
+			if s.err != nil {
+				s.c.Logf("[stage %d] emitted error: %v", params.StageIndex(), s.err)
+				params.Error() <- s.err
+
+				return
+			}
+
+			if s.dropPayloads {
+				s.c.Logf("[stage %d] dropping payload: %v", params.StageIndex(), payload)
+				payload.MarkAsProcessed()
+
+				continue
+			}
+
+			s.c.Logf("[stage %d] emitting payload: %v", params.StageIndex(), payload)
+
+			select {
+			case <-ctx.Done():
+				return
+			case params.Output() <- payload:
+			}
+		}
+	}
+}
+
+// .......
+type sourceStab struct {
+	index int
+	data []pipeline.Payload
+	err error
+}
+
+func (s *sourceStab) Next(ctx context.Context) bool {
+	if s.err != nil || s.index == len(s.data) {
+		return false
+	}
+
+	s.index++
+
+	return true
+}
+
+func (s *sourceStab) Payload() pipeline.Payload {
+	return s.data[s.index-1]
+}
+
+func (s *sourceStab) Error() error {
+	return s.err
+}
+
+// .....
+type sinkStub struct {
+	data []pipeline.Payload
+	err error
+}
+
+func (s *sinkStub) Consume(ctx context.Context, p pipeline.Payload) error {
+	s.data = append(s.data, p)
+
+	return s.err
+}
+
+// .......
+type stringPayload struct {
+	processed bool
+	val string
+}
+
+func (s *stringPayload) Clone() pipeline.Payload {
+	return &stringPayload{
+		val: s.val,
+	}
+}
+
+func (s *stringPayload) MarkAsProcessed() {
+	s.processed = true
+}
+
+func (s *stringPayload) String() string {
+	return s.val
+}
+
+func stringPayloads(numOfValues int) []pipeline.Payload {
+	out := make([]pipeline.Payload, numOfValues)
+	for i := 0; i < len(out); i++ {
+		out[i] = &stringPayload{
+			val: fmt.Sprint(i),
+		}
+	}
+
+	return out
+}
