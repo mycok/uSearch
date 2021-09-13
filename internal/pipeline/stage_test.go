6746a86529fc9696ca2c93ab5d656a1c69f5de3c
myko <awesomeme155@gmail.com>
2021-09-13 16:47:39 +0300

    chore: stage runner tests: dynamic worker pool
    - add TestDynamicWorkerPool test case

--- internal/pipeline/stage_test.go
+++ internal/pipeline/stage_test.go
@@ -58,6 +58,8 @@ func (s *StageTestSuite) TestFixedWorkerPool(c *check.C) {
 		err := p.Process(context.TODO(), src, nil)
 
 		c.Assert(err, check.IsNil)
+		// Close the done channel after p.Process returns but before the worker returns / completes.
+		// Closing the done channels signals it's enclosing worker / go-routine that it's time to exit.
 		close(doneCh)
 	}()
 
@@ -71,13 +73,61 @@ func (s *StageTestSuite) TestFixedWorkerPool(c *check.C) {
 		}
 	}
 
-	// Allow workers to proceed and wait for the pipeline to complete.
+	// Allow workers / go-routines to proceed and wait for the pipeline to complete.
 	close(rendevousCh)
 	select {
 	case <-doneCh:
 	case <-time.After(10 * time.Second):
 		c.Fatal("timed out waiting for pipeline to complete")
 	}
+}
+
+func (s *StageTestSuite) TestDynamicWorkerPool(c *check.C) {
+	numOfWorkers := 5
+	syncCh := make(chan struct{}, numOfWorkers)
+	rendevousCh := make(chan struct{})
+	doneCh := make(chan struct{})
+
+	proc := pipeline.ProcessorFunc(func(c context.Context, p pipeline.Payload) (pipeline.Payload, error) {
+		// Signal that we have reached the sync point and wait for the
+		// green light to proceed by the test code.
+		syncCh <- struct{}{}
+		<-rendevousCh
+
+		return nil, nil
+	})
+
+	src := &sourceStab{data: stringPayloads(numOfWorkers * 2)}
+	p := pipeline.New(pipeline.DynamicWorkerPool(proc, numOfWorkers))
+
+	go func() {
+		err := p.Process(context.TODO(), src, nil)
+
+		c.Assert(err, check.IsNil)
+		// Close the done channel after p.Process returns but before the worker returns / completes.
+		// Closing the done channels signals it's enclosing worker / go-routine that it's time to exit.
+		close(doneCh)
+	}()
+
+	// Wait for all workers to reach sync point. This means that the pool
+	// has scaled up to the max number of workers.
+	for i := 0; i < numOfWorkers; i++ {
+		select {
+		case <-syncCh:
+		case <-time.After(10 * time.Second):
+			c.Fatalf("timed out waiting for worker %d to reach sync point", i)
+		}
+	}
+
+	// Allow workers / go-routines to proceed and process the next batch of records.
+	close(rendevousCh)
+	select {
+	case <-doneCh:
+	case <-time.After(10 * time.Second):
+		c.Fatalf("timed out waiting for pipeline to complete")
+	}
+
+	assertAllProcessed(c, src.data)
 }
 
 // Test suite setup helpers.
