0a4d13ba9566e94be6faab2ee84b8b0583ad913f
myko <awesomeme155@gmail.com>
2021-09-22 03:08:15 +0300

    feat: crawler pipeline: text indexer
    - add textIndexer processor concrete implementation

---
+++ cmd/crawler/text_indexer.go
@@ -0,0 +1,40 @@
+package crawler
+
+import (
+	"context"
+	"time"
+
+	"github.com/mycok/uSearch/internal/textindexer/index"
+	"github.com/mycok/uSearch/internal/pipeline"
+)
+
+// Compile-time check for ensuring textIndexer implements pipeline.Processor.
+var _ pipeline.Processor = (*textIndexer)(nil)
+
+type textIndexer struct {
+	indexer MiniIndexer
+}
+
+func newTextIndexer(indexer MiniIndexer) *textIndexer {
+	return &textIndexer{indexer: indexer}
+}
+
+// Process creates a document instance from the passed payload and indexes the updated / new content
+// by calling Index function.
+func (ti *textIndexer) Process(ctx context.Context, p pipeline.Payload) (pipeline.Payload, error) {
+	payload := p.(*crawlerPayload)
+
+	doc := &index.Document{
+		LinKID:    payload.LinkID,
+		URL:       payload.URL,
+		Title:     payload.Title,
+		Content:   payload.TextContent,
+		IndexedAt: time.Now(),
+	}
+
+	if err := ti.indexer.Index(doc); err != nil {
+		return nil, err
+	}
+	
+	return p, nil
+}
