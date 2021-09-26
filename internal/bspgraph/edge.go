6fc6fffaab2fc4c3ebd55f452970c22cd50bcac6
myko <awesomeme155@gmail.com>
2021-09-26 16:58:44 +0300

    feat: graph based data processing: edge
    - add Edge type

---
+++ internal/bspgraph/edge.go
@@ -0,0 +1,16 @@
+package bspgraph
+
+// Edge represents a directed edge in the Graph.
+type Edge struct {
+	value  interface{}
+	destID string
+}
+
+// DstID returns the vertex ID that corresponds to this edge's target endpoint.
+func (e *Edge) DestID() string { return e.destID }
+
+// Value returns the value associated with this edge.
+func (e *Edge) Value() interface{} { return e.value }
+
+// SetValue sets the provided value to the associated edge.
+func (e *Edge) SetValue(val interface{}) { e.value = val }
