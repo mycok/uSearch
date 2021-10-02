76a90e151a1fe9a912d9763b1876ee681086b3ae
myko <awesomeme155@gmail.com>
2021-10-03 02:52:13 +0300

    feat: graph based data processing: aggregators
    - add int accumulator
    - add accumulator tests

--- internal/bspgraph/aggregator/accumulator.go
+++ internal/bspgraph/aggregator/accumulator.go
@@ -16,7 +16,9 @@ type Float64Accumulator struct {
 func (a *Float64Accumulator) Type() string { return "Float64Accumulator" }
 
 // Get returns the current value of the accumulator.
-func (a *Float64Accumulator) Get() interface{} { return &a.currSum }
+func (a *Float64Accumulator) Get() interface{} { 
+	return loadFloat64(&a.currSum) 
+}
 
 // Set the current value of the accumulator.
 func (a *Float64Accumulator) Set(val interface{}) {
@@ -79,4 +81,50 @@ func (a *Float64Accumulator) Delta() interface{} {
 // loadFloat64 loads, manipulates and returns a raw float64 value.
 func loadFloat64(val *float64) float64 {
 	return math.Float64frombits(atomic.LoadUint64((*uint64)(unsafe.Pointer(val))))
+}
+
+// IntAccumulator implements a concurrent-safe accumulator for int values.
+type IntAccumulator struct {
+	prevSum int64
+	currSum int64
+}
+
+// Type implements bspgraph.Aggregator.
+func (a *IntAccumulator) Type() string { return "IntAccumulator" }
+
+// Get returns the current value of the accumulator.
+func (a *IntAccumulator) Get() interface{} {
+	return int(atomic.LoadInt64(&a.currSum))
+}
+
+// Set the current value of the accumulator.
+func (a *IntAccumulator) Set(val interface{}) {
+	for v64 := int64(val.(int)); ; {
+		oldCurr := a.currSum
+		oldPrevSum := a.prevSum
+		swappedCurr := atomic.CompareAndSwapInt64(&a.currSum, oldCurr, v64)
+		swappedPrev := atomic.CompareAndSwapInt64(&a.prevSum, oldPrevSum, v64)
+
+		if swappedCurr && swappedPrev {
+			return
+		}
+	}
+}
+
+// Aggregate adds an int value to the accumulator.
+func (a *IntAccumulator) Aggregate(val interface{}) {
+	_ = atomic.AddInt64(&a.currSum, int64(val.(int)))
+}
+
+// Delta returns the delta change in the accumulator value since the last time
+// it was invoked or the last time that Set was invoked.
+func (a *IntAccumulator) Delta() interface{} {
+	for {
+		currSum := atomic.LoadInt64(&a.currSum)
+		prevSum := atomic.LoadInt64(&a.prevSum)
+
+		if atomic.CompareAndSwapInt64(&a.prevSum, prevSum, currSum) {
+			return int(currSum - prevSum)
+		}
+	}
 }
