77ea15f5e8664570be89a1b973613ab17034f94f
myko <awesomeme155@gmail.com>
2023-03-06 21:13:42 +0300

    feat: setup cockroach-db kubernetes deployment
    - add and configure docker and make files for cdb

---
+++ deploy/cdb/bootstrap_db.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+ set -eo pipefail
+# Check if both the DB_NAME and DB_HOST arg values have been provided.
+if [[ $# -ne 2 ]]; then
+    echo "Usage: bootstrap DB_NAME DB_HOST"
+    exit 1
+fi
+
+DB_NAME=$1
+DB_HOST=$2
+
+# Write and subsquently append SQL migration steps to a /tmp/migrations-all.sql
+# file.
+echo "CREATE DATABASE IF NOT EXISTS $1; USE $1;" > /tmp/migrations-all.sql
+cat /migrations/*.up* >> /tmp/migrations-all.sql
+
+# Apply the schema in a loop so we can wait until the CDB cluster is ready.
+until cat /tmp/migrations-all.sql | ./cockroach sql --insecure --echo-sql --host $DB_HOST; do
+    sleep 5;
+done
