3d8e6d5565f4fbefec3a52d9caaadf8f4e638ac8
myko <awesomeme155@gmail.com>
2023-04-19 17:41:23 +0300

    refactor html templates to use full links for static web resources

--- monolith/service/pagerank/pagerank.go
+++ monolith/service/pagerank/pagerank.go
@@ -48,7 +48,7 @@ func (svc *Service) Name() string { return "page-rank" }
 func (svc *Service) Run(ctx context.Context) error {
 	svc.config.Logger.WithField(
 		"update_interval", svc.config.UpdateInterval.String(),
-	).Info("starting service")
+	).Info("started service")
 	defer svc.config.Logger.Info("stopped service")
 
 	for {
